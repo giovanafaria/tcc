95af4e78e6479db893ad5f33c4721e0bc536fa48
Giovana Faria <giovanafaria@Giovanas-MacBook-Air-2.local>
2025-05-13 18:56:19 -0300

    adapting manager.py to timing parameters

--- src/reporting/manager.py
+++ src/reporting/manager.py
@@ -24,12 +24,12 @@ class ReportManager:
             "mobility_type": agent.mobility_type.name,  # Type as string
             "start_pos": agent.pos,  # Tuple like (x,y)
             "start_time": self.model.schedule.steps,  # Current step count
-            "end_time": None,  # Will be filled later
-            "distance": 0,  # Cumulative distance traveled
-            "steps": 0,  # Number of movement attempts
-            "evacuated": False,  # Success flag
-            "final_pos": None,  # Add all fields with default values
-            "time_spent": 0
+            "end_time": None,
+            "distance": 0,  # cumulative distance traveled
+            "steps": 0,  # number of movement attempts
+            "evacuated": False,  # success flag  # TODO: colocar tempo para a flag não necessariamente ser true sempre
+            "final_pos": None,  # add all fields with default values
+            "time_spent": 0     # always equals as end_time, bc start_time = 0
         }
         self.data.append(entry)
 
@@ -43,7 +43,7 @@ class ReportManager:
                 entry["steps"] += 1
                 dx = agent.pos[0] - entry["start_pos"][0]
                 dy = agent.pos[1] - entry["start_pos"][1]
-                entry["distance"] = (dx**2 + dy**2)**0.5
+                entry["distance"] = (dx**2 + dy**2)**0.5  # euclidean distance > represent straight line distance (x grid cells)
                 break
 
     def record_evacuation_end(self, agent):
@@ -66,18 +66,29 @@ class ReportManager:
         if not any(entry["evacuated"] for entry in self.data):
             return
         
+        for entry in self.data:
+            if not entry["evacuated"]:
+                agent = next(
+                    (a for a in self.model.schedule.agents
+                    if a.unique_id == entry["agent_id"]),
+                    None
+                )
+                if agent is not None:
+                    entry["end_time"]  = self.model.schedule.steps
+                    entry["final_pos"] = agent.pos
+                    entry["time_spent"] = entry["end_time"] - entry["start_time"]
+
         reports_dir = Path("reports")
+        reports_dir.mkdir(exist_ok=True)  # making sure the dir is ok
 
         timestamp = time.strftime("%Y%m%d-%H%M%S")
         filename = reports_dir / f"evacuation_report_{timestamp}.csv"
 
-        valid_entries = [entry for entry in self.data if entry["evacuated"]]
-
         with open(filename, 'w', newline='') as f:
             writer = csv.DictWriter(f, fieldnames=self.header)
             writer.writeheader()
-            writer.writerows(valid_entries)
-            
+            writer.writerows(self.data)
+        
             # process each agents data
             for entry in self.data:
                 writer.writerow(entry)
